552ed8e820e284e8539b5d140eb734e07959a713
karimSalaheldeen <eng.karimsalaheldeen@gmail.com>
2025-03-13 10:11:25 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpCourseExplainationExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
